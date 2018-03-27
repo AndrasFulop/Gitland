d7b4a1706456f343e4c3952e05a410b6d8925f1b
Gyertya <gyertya@gmail.com>
2018-03-27 10:43:20 +0200

    shout func. created

--- gittie.py
+++ gittie.py
@@ -9,9 +9,13 @@ def add(a, b):
 
 
 def joke():
-    print("The first computer dates back to Adam and Eve. It was an Apple with limited memory, 
-          just one byte. And then everything crashed.")
+    print("The first computer dates back to Adam and Eve. It was an Apple with limited memory," +
+          "just one byte. And then everything crashed.")
     print('That was a joke. Did you get it?')
+
+
+def shout():
+    print("WHAT ARE YOU DOING?\n" * 50)
 
 
 introduce()
@@ -20,3 +24,4 @@ a = int(input('Kerlek adj meg egy szamot!'))
 b = int(input('Most pedig egy masikat szamot!'))
 
 add(a, b)
+shout()
