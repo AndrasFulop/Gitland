982f4b78372f8041ee305e412d14fe22c6ac6926
Gyertya <gyertya@gmail.com>
2018-03-27 10:05:44 +0200

    Function joke() added

--- gittie.py
+++ gittie.py
@@ -1,2 +1,7 @@
 def introduce():
     print("Hello, I'm gittie")
+
+
+def joke():
+    print("The first computer dates back to Adam and Eve. It was an Apple with limited memory, 
+          just one byte. And then everything crashed.")
