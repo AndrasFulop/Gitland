b3fc188ba59473d60e126b3c659e1d2e384416fb
Andras Fulop <andrasfulop86@gmail.com>
2018-03-27 10:14:37 +0200

    add func

--- gittie.py
+++ gittie.py
@@ -1,2 +1,14 @@
 def introduce():
     print("Hello, I'm gittie")
+
+
+def add(a, b):
+    print('A ket szam osszege:', a + b)
+
+
+introduce()
+
+a = int(input('Kerlek adj meg egy szamot!'))
+b = int(input('Most pedig egy masikat szamot!'))
+
+add(a, b)
